55c55204de2d6b4f16cdc92cc4bdca90ad8777cc
PepeM2002 <josemanuelav04@gmail.com>
2024-10-21 15:38:18 -0600

    Este jala

--- bot.py
+++ bot.py
@@ -1,49 +1,45 @@
 from flask import Flask, request
 import settings
-import services  
+import services
 
 app = Flask(__name__)
 
-# Verificación de conexión con Flask
 @app.route('/bienvenido', methods=['GET'])
-def bienvenido():
-    return 'Hola, soy tu chatbot'
+def  bienvenido():
+    return 'Hola mundo bigdateros, desde Flask'
 
-# Verificación de conexión con Webhook
 @app.route('/webhook', methods=['GET'])
 def verificar_token():
     try:
-        token = request.args.get('get_webhook_token')
+        token = request.args.get('hub.verify_token')
         challenge = request.args.get('hub.challenge')
 
-        if token == settings.token and challenge is not None:
+        if token == settings.token and challenge != None:
             return challenge
         else:
-            return 'Error', 403
+            return 'token incorrecto', 403
     except Exception as e:
-        return str(e), 403
-
-# Manejo de mensajes del webhook
+        return e,403
+    
 @app.route('/webhook', methods=['POST'])
-def mensaje():
+def recibir_mensajes():
     try:
         body = request.get_json()
         entry = body['entry'][0]
         changes = entry['changes'][0]
         value = changes['value']
         message = value['messages'][0]
-
         number = message['from']
         messageId = message['id']
         contacts = value['contacts'][0]
         name = contacts['profile']['name']
         text = services.obtener_Mensaje_whatsapp(message)
 
-        services.administrar_bot(text, number, messageId, name)
+        services.administrar_bot(text, number,messageId,name)
         return 'enviado'
-    
+
     except Exception as e:
-        return 'no enviado: ' + str(e)
+        return 'no enviado ' + str(e)
 
 if __name__ == '__main__':
     app.run()
